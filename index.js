aa510d764c729e5db22fb8307dab1b36b37cfc04
Aishwarya5959 <iamaishureddy2004@gmail.com>
2025-04-07 15:28:41 +0530

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
   
-mongoose.connect('mongodb://127.0.0.1:27017/sample')
+mongoose.connect('mongodb://localhost:27017/sample')
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
