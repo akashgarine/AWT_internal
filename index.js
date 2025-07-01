baee5aa22f37ad6ddfef4dce7d644e26b01c9683
akashgarine <122369528+akashgarine@users.noreply.github.com>
2025-07-01 23:28:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
